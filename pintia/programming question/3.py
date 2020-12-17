1319e8381d637ec250a54ffee4597fbd8a422465
matianfang <75879667+matianfang7@users.noreply.github.com>
2020-12-17 19:59:49 +0800

    Update 3.py

--- pintia/programming question/3.py
+++ pintia/programming question/3.py
@@ -44,6 +44,73 @@ n=len(lst)
 for i in range(n):
     if lst[i]==ch1 or lst[i]==ch2:
         print(str(n-i-1)+" "+lst[i])
+
+# 3-4
+
+
+# 3-5
+
+# 3-6
+
+# 3-7
+
+# 3-8
+
+# 3-9
+
+# 3-10
+
+# 3-11
+
+# 3-12
+
+# 3-13
+      
+# 3-14
+
+# 3-15
+
+# 3-16
+Str = list(input())
+Str.sort()
+result = ""
+pre = ""
+for i in Str:
+    if i !=pre:
+        pre = i
+        result+=i
+print(result)
+
+# 3-17
+Str = input().strip()
+ch = input().strip()
+Str=Str.replace(ch.lower(),"").replace(ch.upper(),"")
+print("result: "+str(Str))        
+        
+# 3-18
+Str = input()
+result = ""
+count = 0
+for i in Str:
+    if i.isalpha() and i.lower() not in result and i.upper() not in result :
+        result+=i
+        count+=1
+    if count==10:
+        break
+if count==10:
+    print(result)
+else:
+    print("not found")
+
+# 3-19
+n = int(input())
+maxStr = ""
+for i in range(n):
+    Str=input()
+    if len(Str)>len(maxStr):
+        maxStr=Str
+print("The longest is: "+maxStr)
+
 # 3-20
 num = input()[::-1]
 isZero = True
