bf2fb9e0323c8d1e49edf87a042fbaea32deca75
matianfang <75879667+matianfang7@users.noreply.github.com>
2020-12-17 19:31:31 +0800

    Update 3-3.py

--- pintia/programming question/3-3.py
+++ pintia/programming question/3-3.py
@@ -1,3 +1,40 @@
+# 3-1
+height=input().split(" ")
+sum1=0
+for h in height:
+    h=int(h)
+    sum1+=h
+avg=sum1/len(height)
+for h in height:
+    h=int(h)
+    if h>avg:
+        print(h,end=" ")
+print("")
+
+# 3-2
+weight=(7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2)
+M=('1','0','X','9','8','7','6','5','4','3','2')
+
+n=int(input())
+s=list()
+allPassed = True
+for i in range(n):
+    st=input()
+    lst=list(st)
+    check=[lst[i] for i in range(17) if lst[i]>='0' and lst[i]<='9']
+    if(len(check)!=17):#check if there is a character
+        allPassed = False
+        print(st)
+    else:
+        count=sum([weight[i]*int(st[i]) for i in range(17)])
+        res=count%11
+        if(st[17]!=M[res]):
+            allPassed = False
+            print(st)
+if allPassed:
+    print("All passed")
+
+# 3-3
 s=input()
 ch1,ch2=input().split()
 
@@ -8,3 +45,10 @@ for i in range(n):
     if lst[i]==ch1 or lst[i]==ch2:
         print(str(n-i-1)+" "+lst[i])
 
+#3-21
+s = input()
+print(s)
+if s == s[::-1] :  
+    print("Yes")
+else:
+    print("No")
